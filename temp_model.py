301aa8999bdf2f6417b463facaba0b89f5287558
Nicole Panek <nicoleepanek@gmail.com>
2022-03-11 13:39:35 -0800

    turned .py file into a callable function

--- temp_model.py
+++ temp_model.py
@@ -1,28 +1,50 @@
-#Temporary "model" to test that our GUI is functional
+#Temporary "model" function to test that our GUI is functional
 import pandas as pd
 import numpy as np
 import Bio
 from Bio.Seq import Seq
 from Bio import SeqIO
 
-#read in FASTA file with Thermophllic proteins datasets
 
-thermo_dict = {}
-with open('Examples/2AYQ.fasta') as thermo_fasta_file:  # Will close handle cleanly
-    identifiers = []
-    sequence = []
-    for seq_record in SeqIO.parse(thermo_fasta_file, 'fasta'):  # (generator)
-        identifiers.append(str(seq_record.id))
-        sequence.append(str(seq_record.seq))
-        thermo_dict[str(seq_record.id)] = str(seq_record.seq)
+def temp_model(fasta_file_path):
 
-thermo_list = list(thermo_dict.items())
-df_thermo = pd.DataFrame(thermo_list)
-df_thermo.columns = ['protein','sequence']
+    '''
+    This function was written to test the functionality of our dash GUI
+    It takes the file path to a .fasta file, imports the sequence into a Pd dataframe and
+    returns the dataframe
 
-#convert to csv and save in "Examples" folder
-#If this were the real model, it would return a .csv file 
+    inputs:
+    - fasta_file_path: str; path to .fasta file with protein sequence
+    '''
+
+    #read in FASTA file with Thermophllic proteins datasets
+
+    thermo_dict = {}
+    with open(fasta_file_path) as thermo_fasta_file:  # Will close handle cleanly
+        identifiers = []
+        sequence = []
+        for seq_record in SeqIO.parse(thermo_fasta_file, 'fasta'):  # (generator)
+            identifiers.append(str(seq_record.id))
+            sequence.append(str(seq_record.seq))
+            thermo_dict[str(seq_record.id)] = str(seq_record.seq)
+
+    thermo_list = list(thermo_dict.items())
+    df_thermo = pd.DataFrame(thermo_list)
+    df_thermo.columns = ['protein','sequence']
+
+    #convert to csv and save in "Examples" folder
+    #If this were the real model, it would return a .csv file 
 	#with the classification probabilities
 	
-df_thermo.to_csv('Examples/temp_out.csv', index=True)
-print(df_thermo)
+    #df_thermo.to_csv('temp_out.csv', index=True)
+    return df_thermo
+
+
+
+#-----------------------------------------------------------------------------------
+Example
+#-----------------------------------------------------------------------------------
+
+#df = temp_model('Examples/2AYQ.fasta')
+#print(df)
+
