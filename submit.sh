db3938956e9e7d07a044cd4b2f02c23b937a37b1
J. Gershon <jgershon@mox2.hyak.local>
2022-03-13 08:16:17 -0700

    Training is working

--- submit.sh
+++ submit.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=data_loader_thermodrift
+#SBATCH --job-name=thermodrift
 #SBATCH -p stf
 #SBATCH -A stf
 #SBATCH --nodes=1
-#SBATCH --time=0:02:00
+#SBATCH --time=24:00:00
 #SBATCH --ntasks=1
-#SBATCH --mem=64G
+#SBATCH --mem=100G
+#SBATCH -o ./slurm_out/slurm_%j.out # STDOUT
 
+name='increaseing_width.log'
 
 source activate thermodrift
-python -u test.py >> processing.log
+python -u train_script.py >> $name
 
 
 
