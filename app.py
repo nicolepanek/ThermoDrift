de95ed08236121390365f92439e9c5e826141c62
Jessie Zhao <jesszhao@localhost>
2022-05-05 13:35:02 -0700

    fixed indentation error in line 162

--- app.py
+++ app.py
@@ -159,10 +159,10 @@ def callbacks(_app):
               State('upload-data', 'last_modified'))
 
     def update_output(list_of_contents, list_of_names, list_of_dates):
-    if list_of_contents is not None:
-        children = [
-            parse_contents(c, n, d) for c, n, d in
-            zip(list_of_contents, list_of_names, list_of_dates)]
+        if list_of_contents is not None:
+            children = [
+                parse_contents(c, n, d) for c, n, d in
+                zip(list_of_contents, list_of_names, list_of_dates)]
         return children
 
     # download
